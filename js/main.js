8f41f15776b05ff0099db0bb1adfe7fbe3f5c1c7
Asapanna Rakesh <rakeshark22@gmail.com>
2020-11-18 18:49:19 +0530

    tofix: extra

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//const boxList = document.getElementsByClassName('box')
+
+
+// =================================================================================================================
+// beg: responsive
+// =================================================================================================================
+if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
+    // desktop
+} else {
+    // mob
+    // console.log("makng 49% -> 100%")
+    // for(var i=0; i<boxList.length; i++) {
+    //     boxList[i].style.width = "100%"
+    // }
+}
